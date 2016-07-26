be7b9546a4305610a853eb5e25304eff2e836908
Maryam <mkaka139@gmail.com>
2016-07-26 14:00:59 -0400

    Fixed audioGen to generated audio stream without the arduino

--- Audio/audioGen.m
+++ Audio/audioGen.m
@@ -2,11 +2,10 @@ function [sound, sequence] = audioGen()
     %parameters8
     f1 = 250;                   %reg tone freq
     f2 = 500;                   %odd tone freq
-    toneLength = 0.2;           %lenght of time tone is played (sec)
-    restTime = 0.8;             %length of rest time (sec)
+    toneLength = 0.2;           %length of time tone is played (sec)
+    restTime = 1;               %length of rest time (sec)
     count = 5;                  %number of odd tones 
     thresh = 0.2;               %freq of odd tone
-    s = serial('COM6');         %Arduino Port
     
     %other variables
     fs = 8192;                  
@@ -16,16 +15,13 @@ function [sound, sequence] = audioGen()
     sound = [blank, tone(f1), blank];
     sequence = [f1];
     oddCount = 0; 
-    
-    fopen(s); 
-    
+       
     while true
         if(random('unif', 0, 1) >= thresh)
             f = f1;
         else 
             f = f2; 
             oddCount = oddCount + 1; 
-            fwrite(s, 1); 
         end 
         
         sequence = [sequence f]; 
@@ -34,8 +30,5 @@ function [sound, sequence] = audioGen()
         if(oddCount == count) 
             break;
         end 
-    end 
-    
-    fclose(s);
-    
+    end    
 end 
